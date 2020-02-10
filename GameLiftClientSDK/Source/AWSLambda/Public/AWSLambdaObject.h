947d79bf2208379da5e86c58d3bb71205b9e7d1a
MobiFeast <a.v.gavryschuk@gmail.com>
2020-02-10 18:37:23 +0200

    Wording fixxes

--- GameLiftClientSDK/Source/AWSLambda/Public/AWSLambdaObject.h
+++ GameLiftClientSDK/Source/AWSLambda/Public/AWSLambdaObject.h
@@ -24,31 +24,31 @@ private:
 public:
 
 	/**
-	* public static ULambdaObject::CreateLambdaObjectWithIAMCredentials
+	* public static UAWSLambdaObject::CreateLambdaObjectWithIAMCredentials
 	* Creates a LambdaObject with AWS IAM credentials. This function must be called first before accessing any Lambda client functions.
 	* @param AccessKey [const FString&] AccessKey of your AWS user. @See http://docs.aws.amazon.com/general/latest/gr/managing-aws-access-keys.html
 	* @param Secret [const FString&] SecretKey of your AWS user. @See http://docs.aws.amazon.com/general/latest/gr/managing-aws-access-keys.html
 	* @param Region [const FString&] Default is set to us-east-1 (North Virginia).
-	* @return [ULambdaObject*] Returns UGameLiftClientObject*. Use this to create game sessions, player sessions etc.
+	* @return [UAWSLambdaObject*] Returns UAWSLambdaObject*. Use this to invoke calls to Lambda Functions.
 	**/
 	UFUNCTION(BlueprintCallable, Category = "Lambda Client Object")
 	static UAWSLambdaObject* CreateLambdaObjectWithIAMCredentials(const FString& AccessKey, const FString& Secret, const FString& Region = "us-east-1");
 
 	/**
-	* public static ULambdaObject::CreateLambdaObjectWithIdentityPool
+	* public static UAWSLambdaObject::CreateLambdaObjectWithIdentityPool
 	* Creates a LambdaObject with Cognito Identity Pool. This function must be called first before accessing any Lambda client functions.
 	* @param AccountID [const FString&] AccountID of your AWS account. @See https://docs.aws.amazon.com/general/latest/gr/acct-identifiers.html
 	* @param CognitoIdentityPoolId [const FString&] Cognito Identity Pool Id. @See https://docs.aws.amazon.com/cognito/latest/developerguide/identity-pools.html
-	* @return [ULambdaObject*] Returns UGameLiftClientObject*. Use this to create game sessions, player sessions etc.
+	* @return [UAWSLambdaObject*] Returns UAWSLambdaObject*. Use this to invoke calls to Lambda Functions.
 	**/
 	UFUNCTION(BlueprintCallable, Category = "Lambda Client Object")
 	static UAWSLambdaObject* CreateLambdaObjectWithIdentityPool(const FString& AccountID, const FString& CognitoIdentityPoolId, const FString& Region = "us-east-1");
 
 	/**
-	* public ULambdaFunction::CreateLambdaFunction
+	* public UAWSLambdaFunction::CreateLambdaFunction
 	* Creates a LambdaFunction Object. To Make Calls to AWS Lambda
 	* @param LambdaFunctionName [FString] Lambda Function Name from your AWS account.
-	* @return [ULambdaFunction*] Returns ULambdaFunction*. Use this to Call the function itself and manage response.
+	* @return [UAWSLambdaFunction*] Returns UAWSLambdaFunction*. Use this to Call the function itself and manage response.
 	**/
 	UFUNCTION(BlueprintCallable, Category = "Lambda Client Object")
 	UAWSLambdaFunction* CreateLambdaFunction(FString LambdaFunctionName);
